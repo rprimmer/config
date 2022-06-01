af20b83e925eff496344417da0174282b8eebfe7
Robert Primmer <robert.primmer@gmail.com>
2022-06-01 12:14:05 -0400

    Updated make-project.sh for makefile selection

--- make-project.sh
+++ make-project.sh
@@ -1,23 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Create and populate a new C project folder
 
-[[ $# -eq 0 ]] && { echo "Usage: `basename $0` project-name";  exit 1; }
+[[ $# -lt 2 ]] && { echo "Usage: `basename $0` project-name light | full";  exit 1; }
 
-[[ -e $1 ]] && { echo "Project folder $1 already exists";  exit 1; }
+# Use "light" or "full" as arg 2 to indicate which version of the makefile to use
+
+if [ "$2" = "light" ] || [ "$2" = "full" ] ; then
+    MAKE=$2
+else
+    echo "Error: argument 2 must be either 'light' or 'full'"
+    echo "Usage: `basename $0` project-name light | full"
+    exit 1
+fi
+
+[[ -e $1 ]] && { echo "Error: project folder $1 already exists";  exit 1; }
 
 PROJECT=$1
+
 CONFIG=${HOME}/ic/linux/config
 
 mkdir -p ${PROJECT}/bin ${PROJECT}/obj ${PROJECT}/src ${PROJECT}/.vscode 
 
 cd ${PROJECT}
 
-cp ${CONFIG}/makefile .
+cp ${CONFIG}/makefile-${MAKE} ./makefile
+cp ${CONFIG}/C.gitignore ./.gitignore
 
 echo \# Project ${PROJECT} > README.md 
-
-echo -en "*.dSYM\n*.o\n*.so\n*.dylib\n*.a\n${PROJECT}" > .gitignore
 
 cd .vscode 
 
