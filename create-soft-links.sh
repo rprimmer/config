39f2867a41a6e5bfd5bbc2ab9cc4403f9bce9b88
Robert Primmer <robert.primmer@gmail.com>
2024-01-21 11:24:45 -0500

    Updates for Documents dir location instead of iCloud

--- create-soft-links.sh
+++ create-soft-links.sh
@@ -2,21 +2,22 @@
 
 # Configure home directory soft links for new system set up
 pushd ${HOME} || return 
-ln -fs ${HOME}/Library/Mobile\ Documents/com~apple~CloudDocs/ ic
 
-ln -fs ic/linux/config/bash_logout    .bash_logout
-ln -fs ic/linux/config/bash_profile   .bash_profile 
-ln -fs ic/linux/config/bashrc         .bashrc 
-ln -fs ic/linux/config/dircolors      .dircolors 
-ln -fs ic/linux/config/editreadrc     .editreadrc
-ln -fs ic/linux/config/emacs/init.el  .emacs 
-ln -fs ic/linux/config/gitconfig      .gitconfig 
-ln -fs ic/linux/config/inputrc        .inputrc
-ln -fs ic/linux/config/ssh            .ssh
-ln -fs ic/linux/config/vimrc          .vimrc
-ln -fs ic/linux/config/emacs          .xemacs
-ln -fs ic/Swift                       Swift 
-ln -fs ic/linux/bin                   bin 
-ln -fs ic/Downloads                   Downloads
+# The Downloads dir stays in iCloud folder
+# ln -fsv ic/Downloads Downloads
+
+# The remaining files are now in ~/Downloads/linux 
+ln -fsv ${HOME}/Documents/linux/config/bash_logout    .bash_logout
+ln -fsv ${HOME}/Documents/linux/config/bash_profile   .bash_profile 
+ln -fsv ${HOME}/Documents/linux/config/bashrc         .bashrc 
+ln -fsv ${HOME}/Documents/linux/config/dircolors      .dircolors 
+ln -fsv ${HOME}/Documents/linux/config/editreadrc     .editreadrc
+ln -fsv ${HOME}/Documents/linux/config/emacs/init.el  .emacs 
+ln -fsv ${HOME}/Documents/linux/config/gitconfig      .gitconfig 
+ln -fsv ${HOME}/Documents/linux/config/inputrc        .inputrc
+ln -fsv ${HOME}/Documents/linux/config/ssh            .ssh
+ln -fsv ${HOME}/Documents/linux/config/vimrc          .vimrc
+ln -fsv ${HOME}/Documents/linux/config/emacs          .xemacs
+ln -fsv ${HOME}/Documents/linux/bin                   bin 
 
 popd 
